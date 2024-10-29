7596dbd0dd539a5045d86d327c0075cd21ec9c40
Persephone Wilder <shaneawilder@gmail.com>
2024-10-29 13:29:57 -0600

    Added a getScore and makeRestaurant methods

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,19 @@
 class restaurant:
-        def __init__(self, name, score):
+        def __init__(self):
+                self.score = None
+                self.name = None
+                self.type = None
+                self.address = None
+        def __str__(self):
+                str = "{0} serves {1} food, it has a score of {2} and it's at {3}".format(self.name, self.type, self.score, self.address)
+                return str
+        
+        def getScore(self, score1=0, score2=0):
+                score = score1 + score2
+                return score
+        
+        def makeRestaurant(self, score=0, name="", type="", address=""):
                 self.score = score
                 self.name = name
+                self.type = type
+                self.address = address
